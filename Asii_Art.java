0e2af292fc17398452c98abc567c78a43af9b8ad
trevorkw7 <54345106+trevorkw7@users.noreply.github.com>
2019-09-16 08:27:37 -0700

    ASCII Art Project 1
    Prints a series of playing cards in ascending order

---
+++ Asii_Art.java
@@ -0,0 +1,48 @@
+
+public class Asii_Art {
+
+	//prints title
+	public static void deckTitle()
+	{
+		System.out.println("Welcome! Here's your deck");
+	}
+	
+	
+	//prints card by passing by using the "cardType" parameter
+	public static void cardCreator(String cardType){ 
+		
+		//generic card template 
+		System.out.println(".------.");
+		System.out.println("|" + cardType + ".--. |");
+		System.out.println("| :/\\: |");
+		System.out.println("| :\\/: |");
+		System.out.println("| '--'" + cardType + "|");
+		System.out.println("\"------\"");
+		
+		
+	}
+	
+	
+	public static void main (String[] args) {
+		
+		//call deckTitle method to print title
+		deckTitle();
+		
+		//call cardCreator method and pass cardType argument to print card to console
+		cardCreator("A");
+		cardCreator("2");
+		cardCreator("3");
+		cardCreator("4");
+		cardCreator("5");
+		cardCreator("6");
+		cardCreator("7");
+		cardCreator("8");
+		cardCreator("9");
+		cardCreator("10");
+		cardCreator("J");
+		cardCreator("Q");
+		cardCreator("K");
+		
+	}
+	
+}
